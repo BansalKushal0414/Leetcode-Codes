dda55a86d5f09375fe84162025898982d9b692e5
BansalKushal0414 <157558779+BansalKushal0414@users.noreply.github.com>
2026-01-29 21:02:23 +0530

    Time: 4 ms (62.19%) | Memory: 45.2 MB (70.79%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,16 +1,38 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+
+        // for(int i = 0;i<nums.length;i++){
+        //     hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        // }
+
+        // for(int num : hm.keySet()){
+        //     if(hm.get(num) == 1){
+        //         return num;
+        //     }
+        // }
+        // return nums[0];
+
+        int[] bits = new int[32];
 
         for(int i = 0;i<nums.length;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-
-        for(int num : hm.keySet()){
-            if(hm.get(num) == 1){
-                return num;
+            for(int j = 0;j<32;j++){
+                if((nums[i] & (1 << j)) != 0){
+                    bits[j]++;
+                }
             }
         }
-        return nums[0];
+
+        for(int i= 0;i<32;i++){
+            bits[i] = bits[i]%3;
+        }
+
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bits[i] == 1) {
+                result |= (1 << i);
+            }
+        }
+    return result;
     }
 }

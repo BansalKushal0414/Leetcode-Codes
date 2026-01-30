4ca7e3172bfb000ecde54cff46e2fb8b25e58afa
BansalKushal0414 <157558779+BansalKushal0414@users.noreply.github.com>
2026-01-30 12:28:12 +0530

    Time: 3 ms (32.31%) | Memory: 48.8 MB (43.70%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0){
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+    public int[] countBits(int n) {
+        int arr[] = new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            arr[i] = hammingWeight(i);
+        }
+        return arr;
+    }
+}

551cd60d1bc32ca6fb88254e37bca25a3d6c0254
BansalKushal0414 <157558779+BansalKushal0414@users.noreply.github.com>
2026-03-13 17:46:59 +0530

    Time: 19 ms (9.55%) | Memory: 41.7 MB (97.98%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,14 @@
 class Solution {
+    int[] memo = new int[31];
+    // public int fib(int n) {
+    //     if(n==0) return 0;
+    //     if(n==1) return 1;
+    //     return fib(n-1) + fib(n-2);
+    // }
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        if(n==0) return memo[n] = 0;
+        if(n==1) return memo[n] = 1;
+        memo[n] = fib(n-1) + fib(n-2);
+        return memo[n];
     }
 }

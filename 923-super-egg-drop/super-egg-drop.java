6082848833dadd4a6d1dbe4e01571879f4ddd0a0
BansalKushal0414 <157558779+BansalKushal0414@users.noreply.github.com>
2026-03-14 13:25:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (86.90%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,42 +1,18 @@
 class Solution {
 
-    int[][] memo;
-
     public int superEggDrop(int k, int n) {
-        memo = new int[k + 1][n + 1];
-        return solve(k, n);
-    }
 
-    private int solve(int k, int n) {
+        int[] dp = new int[k + 1];
+        int moves = 0;
 
-        if (n == 0 || n == 1) return n;
-        if (k == 1) return n;
+        while (dp[k] < n) {
+            moves++;
 
-        if (memo[k][n] != 0) return memo[k][n];
-
-        int left = 1;
-        int right = n;
-        int ans = Integer.MAX_VALUE;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            int breakCase = solve(k - 1, mid - 1);
-            int notBreak = solve(k, n - mid);
-
-            int worst = 1 + Math.max(breakCase, notBreak);
-
-            ans = Math.min(ans, worst);
-
-            if (breakCase > notBreak) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            for (int i = k; i >= 1; i--) {
+                dp[i] = dp[i] + dp[i - 1] + 1;
             }
         }
 
-        memo[k][n] = ans;
-        return ans;
+        return moves;
     }
 }
